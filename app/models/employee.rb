4c16616e27f92abfa5b39ae496f435bbabad6695
Armando Mariscal <armando.marisscal@gmail.com>
2026-04-30 23:37:32 -0600

    feat(employee): add status enum and select input

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,10 @@
 class Employee < ApplicationRecord
+    enum :status, {
+        active: "active",
+        inactive: "inactive"
+    }
+
     validates :name, presence: true
     validates :salary_base, numericality: { greater_than_or_equal_to: 0 }
+    validates :status, presence: true
 end
