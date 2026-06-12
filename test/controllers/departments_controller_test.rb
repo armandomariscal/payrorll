03f2a3ad296919381a7f34cd491200822e0a4cd7
Armando Mariscal <armando.marisscal@gmail.com>
2026-06-11 22:58:46 -0600

    test(departments): cover select list endpoint

--- test/controllers/departments_controller_test.rb
+++ test/controllers/departments_controller_test.rb
@@ -39,4 +39,73 @@ class DepartmentsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_response :no_content
   end
+
+  test "should get departments select list" do
+    Department.create!(
+      name: "Information Technology",
+      code: "IT"
+    )
+
+    get departments_select_list_url
+
+    assert_response :success
+  end
+
+  test "select list returns formatted departments" do
+    department = Department.create!(
+      name: "Information Technology",
+      code: "IT"
+    )
+
+    get departments_select_list_url
+
+    json = JSON.parse(response.body)
+
+    assert_equal 1, json.size
+    assert_equal department.id, json[0]["id"]
+    assert_equal department.name, json[0]["value"]
+  end
+
+  test "select list returns formatted departments" do
+    department = Department.create!(
+      name: "Information Technology",
+      code: "IT"
+    )
+
+    get departments_select_list_url
+
+    json = JSON.parse(response.body)
+
+    assert_equal 1, json.size
+    assert_equal department.id, json[0]["id"]
+    assert_equal department.name, json[0]["value"]
+  end
+
+  test "select list returns formatted departments" do
+    department = Department.create!(
+      name: "Information Technology",
+      code: "IT"
+    )
+
+    get departments_select_list_url
+
+    json = JSON.parse(response.body)
+
+    assert_equal 1, json.size
+    assert_equal department.id, json[0]["id"]
+    assert_equal department.name, json[0]["value"]
+  end
+
+  test "select list is ordered by name" do
+    Department.create!(name: "Sales", code: "SALES")
+    Department.create!(name: "Accounting", code: "ACC")
+
+    get departments_select_list_url
+
+    json = JSON.parse(response.body)
+
+    assert_equal "Accounting", json[0]["value"]
+    assert_equal "Sales", json[1]["value"]
+  end
+
 end
